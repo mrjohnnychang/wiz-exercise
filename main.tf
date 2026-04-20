f6e1dc04f10334f929710fa2332d7cfc84888c8e
mrjohnnychang <14340684+mrjohnnychang@users.noreply.github.com>
2026-04-20 10:01:33 -0700

    Update Helm authentication API to v1 for K8s 1.35 compatibility

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ provider "helm" {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
+      api_version = "client.authentication.k8s.io/v1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
     }
