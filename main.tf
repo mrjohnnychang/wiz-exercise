87d42e763c4a204a66a2dc98e855d974d860b023
mrjohnnychang <14340684+mrjohnnychang@users.noreply.github.com>
2026-04-20 10:19:04 -0700

    Fix Helm credentials by passing explicit AWS region

--- main.tf
+++ main.tf
@@ -54,7 +54,11 @@ provider "helm" {
   kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    exec = {
+      api_version = "client.authentication.k8s.io/v1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", "us-west-2"]
+      command     = "aws"
+    }
   }
 }
 
