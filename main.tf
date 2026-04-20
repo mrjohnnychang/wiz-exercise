4683e86fa2bf11230f11506cbb2242464d3f66bf
mrjohnnychang <14340684+mrjohnnychang@users.noreply.github.com>
2026-04-20 10:33:14 -0700

    Switch Helm provider to use native Terraform EKS token

--- main.tf
+++ main.tf
@@ -54,11 +54,7 @@ provider "helm" {
   kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", "us-west-2"]
-      command     = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
