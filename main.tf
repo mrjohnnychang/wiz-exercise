90f14adcd434e241712d4c5e7c20bb0f0458fad8
mrjohnnychang <14340684+mrjohnnychang@users.noreply.github.com>
2026-04-24 12:11:46 -0700

    Fix: Added GetBucketAcl to CloudTrail S3 policy

--- main.tf
+++ main.tf
@@ -138,17 +138,25 @@ resource "aws_s3_bucket" "cloudtrail_logs" {
   force_destroy = true
 }
 
+# policy to allow cloudtrail to write to the cloudtrail logs bucket
 resource "aws_s3_bucket_policy" "cloudtrail_policy" {
   bucket = aws_s3_bucket.cloudtrail_logs.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "AWSCloudTrailWrite",
-        Effect = "Allow",
-        Principal = { Service = "cloudtrail.amazonaws.com" },
-        Action = "s3:PutObject",
-        Resource = "${aws_s3_bucket.cloudtrail_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Sid       = "AWSCloudTrailAclCheck"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:GetBucketAcl"
+        Resource  = aws_s3_bucket.cloudtrail_logs.arn
+      },
+      {
+        Sid       = "AWSCloudTrailWrite"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.cloudtrail_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
         Condition = { StringEquals = { "s3:x-amz-acl" = "bucket-owner-full-control" } }
       }
     ]
